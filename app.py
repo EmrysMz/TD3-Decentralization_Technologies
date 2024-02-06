fa4bb17c99ade748e8114a5720a7515bdc38d9c7
Emrys MEZIANI <emrys.meziani@edu.devinci.fr>
2024-02-06 20:05:53 +0100

    add API

--- app.py
+++ app.py
@@ -4,54 +4,87 @@ import pickle
 import numpy as np
 
 
-
 app = Flask(__name__)
 CORS(app)
 
-with open('rf_classifier_model.pkl', 'rb') as file:
-    model = pickle.load(file)
-    
-with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)
+# Load the models
+with open('K-Nearest_Neighbors_model.pkl', 'rb') as file:
+    knn_model = pickle.load(file)
+
+with open('Random_Forest_model.pkl', 'rb') as file:
+    rf_model = pickle.load(file)
+
+with open('Support_Vector_Machine_model.pkl', 'rb') as file:
+    svm_model = pickle.load(file)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('indexe.html')
 
+@app.route('/predict', methods=['POST'])
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         features = request.form.to_dict(flat=False)
+
+        # Extract feature values
+        sex = int(features['sex'][0])
+        age = float(features['age'][0])
+        fare = float(features['fare'][0])
+        adult_male = int(features['adult_male'][0])
+        pclass = int(features['pclass'][0])
+
+        if pclass == 1:
+            pclass_1 = 1
+            pclass_2 = 0
+            pclass_3 = 0
+        elif pclass == 2:
+            pclass_1 = 0
+            pclass_2 = 1
+            pclass_3 = 0
+        else:
+            pclass_1 = 0
+            pclass_2 = 0
+            pclass_3 = 1
+
+        # Create feature array
+        feature_array = np.array([[sex, age, fare, adult_male, pclass_1, pclass_2, pclass_3]])
+
+        # Make the predictions
+        knn_prediction = knn_model.predict(feature_array)
+        rf_prediction = rf_model.predict(feature_array)
+        svm_prediction = svm_model.predict(feature_array)
+
+        # Convert predictions to readable labels
+        prediction_dict = {
+            0: 'Dead',
+            1: 'Alive'
+        }
+
+        knn_prediction = prediction_dict[knn_prediction[0]]
+        rf_prediction = prediction_dict[rf_prediction[0]]
+        svm_prediction = prediction_dict[svm_prediction[0]]
         
-        print(features)
-        feature_values = [float(value) for value in features['value']]
-
-        feature_array = np.array([feature_values])
+        print(feature_array)
         
+        print(knn_prediction, rf_prediction, svm_prediction)
 
-        scaled_feature_array = scaler.transform(feature_array)
-
-        prediction = model.predict(scaled_feature_array)
-        prediction_list = prediction.tolist()
-        
-        if prediction_list[0] == 0:
-            prediction_list[0] = 'Not popular'
-        elif prediction_list[0] == 1:
-            prediction_list[0] = 'Popular'
-        print(prediction_list)
-
-        return render_template('index.html', prediction=prediction_list[0])
+        return render_template(
+            'indexe.html',
+            knn_prediction=knn_prediction,
+            rf_prediction=rf_prediction,
+            svm_prediction=svm_prediction,
+            form_data=features  # Pass the form data back to the template
+        )
 
     except Exception as e:
-        return render_template('index.html', error=str(e))
-
+        return render_template('indexe.html', error=str(e))
 
 @app.after_request
 def add_header(response):
     response.headers['Cache-Control'] = 'no-store'
     return response
-
-
 
 if __name__ == '__main__':
     host = '0.0.0.0'
